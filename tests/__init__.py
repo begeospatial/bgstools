5df71f1be19055cc6fd0da01c3bccd5cc528d1b5
José Beltrán <102664984+jose-begeospatial@users.noreply.github.com>
2023-05-19 13:22:17 +0200

    added datastorage module

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,8 @@
 import os
 import pytest
-from bgstools2.io import load_yaml
-from bgstools2.utils import create_subdirectory
-from bgstools2.spatial import get_h3_geohash, reproject_coordinates, get_h3_geohash_epsg3006, get_coordinates_epsg3006_from_geohash
+from bgstools.io import load_yaml
+from bgstools.utils import create_subdirectory
+from bgstools.spatial import get_h3_geohash, reproject_coordinates, get_h3_geohash_epsg3006, get_coordinates_epsg3006_from_geohash
 
 
 # ------------------------------------------------------------
