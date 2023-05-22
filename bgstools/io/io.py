fea266a97e392b99810f9f4f18e6c45e83ab41a6
José Beltrán <102664984+jose-begeospatial@users.noreply.github.com>
2023-05-22 18:22:52 +0200

    v0.1.2 added streamlit tools

--- bgstools/io/io.py
+++ bgstools/io/io.py
@@ -1,26 +1,59 @@
 import os
 import yaml
+from collections import OrderedDict
 
 
 def load_yaml(filepath: str) -> dict:
-    """Load a YAML file from a given file path and return a dictionary object containing the parsed data.
+    """
+    Loads a YAML file.
 
-    Parameters:
-    filepath (str): The file path to the YAML file to be loaded.
+    Can be used as stand-alone script by providing a command-line argument:
+        python load_yaml.py --filepath /file/path/to/filename.yaml
+
+    Args:
+        filepath (str): The absolute path to the YAML file.
 
     Returns:
-    dict: A dictionary object containing the parsed data.
+        dict: The contents of the YAML file as a dictionary.
 
     Raises:
-    IOError: If the file path does not exist or if there is an error loading the file.
+        FileNotFoundError: If the file does not exist.
+        yaml.YAMLError: If there is an error while loading the YAML file.
     """
     if not os.path.isfile(filepath):
-        raise IOError(f"`filepath` does not exist = `{filepath}`")
+        raise FileNotFoundError(f"No such file or directory: '{filepath}'")
 
-    try:
-        with open(filepath, 'r') as file_descriptor:
+    with open(filepath, 'r') as file_descriptor:
+        try:
             yaml_data = yaml.safe_load(file_descriptor)
-    except Exception as e:
-        raise IOError(f"Error loading file `{filepath}`: {str(e)}")
+        except yaml.YAMLError as msg:
+            raise yaml.YAMLError(f'File `{filepath}` loading error. \n {msg}')
+        else:
+            return yaml_data
 
-    return yaml_data
+def get_available_services(services_filepath: str) -> OrderedDict:
+    """
+    Retrieves available services from a yaml file. These services can be used to
+    create a multi-page app using Streamlit. 
+
+    Args:
+        services_filepath (str): The absolute path to the yaml file containing the services.
+
+    Returns:
+        OrderedDict: An ordered dictionary of services if any are available. 
+                     The dictionary is ordered based on the order of services in the yaml file.
+                     Each key-value pair corresponds to a service name and its associated information.
+                     Returns None if the yaml file does not contain any services.
+    Raises:
+        FileNotFoundError: If the services_filepath does not exist.
+    """
+    if not os.path.isfile(services_filepath):
+        raise FileNotFoundError(f"No such file or directory: '{services_filepath}'")
+
+    available_services = load_yaml(filepath=os.path.abspath(services_filepath))
+
+    if available_services:
+        services_dict = OrderedDict({service['name']: service for service in available_services})
+        return services_dict
+
+    return None
---
+++ tests/io_tests.py
@@ -0,0 +1,52 @@
+import unittest
+from collections import OrderedDict
+from bgstools.io import load_yaml, get_available_services, script_as_module
+
+
+class TestFunctions(unittest.TestCase):
+
+    def test_load_yaml(self):
+        valid_yaml_path = "/path/to/valid/yaml"
+        invalid_yaml_path = "/path/to/invalid/yaml"
+        nonexistent_path = "/nonexistent/path"
+
+        # Test valid case
+        self.assertIsInstance(load_yaml(valid_yaml_path), dict)
+
+        # Test invalid case
+        with self.assertRaises(yaml.YAMLError):
+            load_yaml(invalid_yaml_path)
+
+        # Test nonexistent file
+        with self.assertRaises(FileNotFoundError):
+            load_yaml(nonexistent_path)
+
+    def test_get_available_services(self):
+        valid_services_path = "/path/to/valid/services"
+        nonexistent_path = "/nonexistent/path"
+
+        # Test valid case
+        self.assertIsInstance(get_available_services(valid_services_path), OrderedDict)
+
+        # Test nonexistent file
+        with self.assertRaises(FileNotFoundError):
+            get_available_services(nonexistent_path)
+
+    def test_script_as_module(self):
+        valid_module_path = "/path/to/valid/module"
+        valid_services_path = "/path/to/valid/services"
+        nonexistent_path = "/nonexistent/path"
+
+        # Test valid case
+        self.assertTrue(script_as_module(valid_module_path, valid_services_path))
+
+        # Test invalid module path
+        with self.assertRaises(FileNotFoundError):
+            script_as_module(nonexistent_path, valid_services_path)
+
+        # Test invalid services path
+        with self.assertRaises(NotADirectoryError):
+            script_as_module(valid_module_path, nonexistent_path)
+
+if __name__ == "__main__":
+    unittest.main()
